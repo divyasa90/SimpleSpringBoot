5b2d75a5f9ac1bfc11cbba943a5ff2f03a546d73
divyasa90 <ddstudy90@gmail.com>
2024-03-25 10:53:03 -0500

    commit on all changes

--- src/main/java/com/simple/spring/boot/App.java
+++ src/main/java/com/simple/spring/boot/App.java
@@ -1,13 +1,25 @@
 package com.simple.spring.boot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) /*{
+        SpringApplication.run(App.class, args);
+    }*/
+        {
+//           kill $(lsof -t -i:8080)
+            SpringApplication app = new SpringApplication(App.class);
+            app.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
+            app.run(args);
+        }
+
 }
